defdeca3b65db79f56b90c9c238ba28924891357
agray <adam.gray@qa.com>
2024-11-14 14:40:12 +0000

    complete Task2

---
+++ Task2/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+docker rm -f $(docker ps -qa)
+docker network create trio-task-network || sleep 1
+docker volume create new-volume || sleep 1
+docker build -t trio-task-mysql:5.7 db
+docker build -t trio-task-flask-app:latest flask-app
+
+docker run -d \
+    -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
+    --name mysql \
+    --network trio-task-network \
+    trio-task-mysql:5.7
+
+docker run -d \
+    -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
+    --name flask-app \
+    --network trio-task-network \
+    trio-task-flask-app:latest
+
+docker run -d \
+    --name nginx \
+    -p 80:80 \
+    --network trio-task-network \
+    --mount type=bind,source=$(pwd)/nginx/nginx.conf,target=/etc/nginx/nginx.conf \
+    nginx:latest
+
+sleep 5
+docker ps -a
